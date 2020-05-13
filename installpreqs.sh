8b330340a89037bd5495365d6f28f85f746d5946
ofek-b <53834911+ofek-b@users.noreply.github.com>
2020-05-13 23:51:13 +0300

    Update installpreqs.sh

--- installpreqs.sh
+++ installpreqs.sh
@@ -1,4 +1,4 @@
 wget http://ftp.de.debian.org/debian/pool/main/s/sfst/sfst_1.4.7b-1+b1_amd64.deb
-apt install ./sfst_1.4.7b-1+b1_amd64.deb
+sudo apt install ./sfst_1.4.7b-1+b1_amd64.deb
 rm sfst_1.4.7b-1+b1_amd64.deb
-pip3 install beautifulsoup4
+pip install beautifulsoup4
